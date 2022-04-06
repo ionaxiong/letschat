76a9a6ea474e8ea8956152c36d4bae62d8bfc775
Ming Xiong <ming.xiong@myy.haaga-helia.fi>
2022-04-07 00:48:07 +0300

    update flatlist style

--- screens/ChatsScreen.tsx
+++ screens/ChatsScreen.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
-import ChatListItem from '../components/ChatListItem';
-import { Text, View } from '../components/Themed';
-import ChatRooms from '../data/ChatRooms';
-
+import { FlatList, StyleSheet } from "react-native";
+import ChatListItem from "../components/ChatListItem";
+import { Text, View } from "../components/Themed";
+import ChatRooms from "../data/ChatRooms";
 
 export default function ChatsScreen() {
   return (
-    <View style={styles.container}> 
-    <FlatList
-     data={ChatRooms}
-     renderItem={({item}) => <ChatListItem chatRoom={item} />}
-    >
-    </FlatList>
+    <View style={styles.container}>
+      <FlatList
+        style={styles.flatList}
+        keyExtractor={(item) => item.id}
+        data={ChatRooms}
+        renderItem={({ item }) => <ChatListItem chatRoom={item} />}
+      ></FlatList>
     </View>
   );
 }
@@ -20,7 +19,11 @@ export default function ChatsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  flatList: {
+    width: "100%",
+    marginRight: 5,
   },
 });
