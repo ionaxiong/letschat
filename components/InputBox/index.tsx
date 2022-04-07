a898de837be95db586b035016efe6c296afed035
Ming Xiong <ming.xiong@myy.haaga-helia.fi>
2022-04-08 00:24:53 +0300

    set input message state

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, TextInput } from "react-native";
 import styles from "./styles";
+import { useState } from "react";
 import {
   MaterialCommunityIcons,
   FontAwesome5,
@@ -9,13 +10,30 @@ import {
 } from "@expo/vector-icons";
 
 const InputBox = () => {
+  const [message, setMessage] = useState("");
+
   return (
     <View style={styles.container}>
       <View style={styles.mainContainer}>
         <FontAwesome5 name="laugh-beam" size={24} color="grey"></FontAwesome5>
-        <TextInput style={styles.textInput} multiline/>
-        <Entypo name="attachment" size={24} color="grey" style={styles.icon} ></Entypo>
-        <Fontisto name="camera" size={24} color="grey" style={styles.icon} ></Fontisto>
+        <TextInput
+          style={styles.textInput}
+          multiline
+          value={message}
+          onChangeText={setMessage}
+        />
+        <Entypo
+          name="attachment"
+          size={24}
+          color="grey"
+          style={styles.icon}
+        ></Entypo>
+        <Fontisto
+          name="camera"
+          size={24}
+          color="grey"
+          style={styles.icon}
+        ></Fontisto>
       </View>
       <View style={styles.buttonContainer}>
         <MaterialCommunityIcons
