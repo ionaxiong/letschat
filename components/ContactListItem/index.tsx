84830bc67bcd9c49c8847a9605ba193b8f2f8ef1
Ming Xiong <ming.xiong@myy.haaga-helia.fi>
2022-04-13 19:56:10 +0300

    implement contactlistitem component to add contact to chatroom

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -1,7 +1,12 @@
 import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 import { User } from "../../types";
-import styles from './styles';
+import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { API, graphqlOperation } from "aws-amplify";
+import {
+  createChatRoom,
+  createChatRoomUser,
+} from "../../src/graphql/mutations";
 
 export type ContactListItemProps = {
   user: User;
@@ -12,8 +17,20 @@ const ContactListItem = (props: ContactListItemProps) => {
 
   const navigation = useNavigation();
 
-  const onClick = () => {
+  const onClick = async () => {
     // navigate to the chat room with this user
+    try {
+      //create a new chatroom
+      const newChatRoomData = await API.graphql(
+        graphqlOperation(createChatRoom, { input: {} })
+      );
+      console.log(newChatRoomData);
+      //add the user to the chatroom
+
+      //add the authenticated user to the chatroom
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -23,7 +40,7 @@ const ContactListItem = (props: ContactListItemProps) => {
           <Image source={{ uri: user.imageUri }} style={styles.avatar}></Image>
           <View style={styles.midContainer}>
             <Text style={styles.username}>{user.name}</Text>
-            <Text style={styles.status} > {user.status} </Text>
+            <Text style={styles.status}> {user.status} </Text>
           </View>
         </View>
       </View>
